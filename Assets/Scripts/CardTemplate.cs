9882b6d5ec11eb04fb10d7cdf25fa59a088c1b0b
Saruto <saruto713@gmail.com>
2018-08-17 18:48:30 -0400

    created template card prefab

---
+++ Assets/Scripts/CardTemplate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardTemplate : MonoBehaviour {
+
+	// References to the various GOs on the template.
+	public Image Background;
+	public Image ManaCostIcon;
+	public Image UpArrow;
+	public Image DownArrow;
+	public Image LeftArrow;
+	public Image RightArrow;
+	public Image AttackIcon;
+	public Image HealthIcon;
+
+	public Text CardName;
+	public Text CardText;
+	public Text ManaCostText;
+	public Text AttackText;
+	public Text HealthText;
+
+
+}
