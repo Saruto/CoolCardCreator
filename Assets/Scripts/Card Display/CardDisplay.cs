e943e83f17b81479b7b7d662b0fa299b9f2e3bb3
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-11-13 16:48:27 -0500

    Added a deck display thing

---
+++ Assets/Scripts/Card Display/CardDisplay.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CardDisplay : MonoBehaviour {
+
+    CSVParser parser;
+
+    // Used for displaying the deck
+    [SerializeField] GameObject deckDisplay;
+    // Each card image
+    GameObject[] CardImages = new GameObject[8];
+
+    // Used for tracking the offset of cards, for displaying the cards in the card display. Offset by values of 8
+    int offset = 0;
+
+	// Use this for initialization
+	void Start () {
+        // Find the parser to get access to the card list
+        // Also eventual stop using .Find lol
+        parser = GameObject.Find("Parser").GetComponent<CSVParser>();
+
+        // Find each card image, and set them equal to the children of the card grid
+        for (int i = 0; i < CardImages.Length; i++)
+        {
+            CardImages[i] = transform.GetChild(i).gameObject;
+        }
+        // Display the card names
+        UpdateCards();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateCards()
+    {
+        int length;
+
+        // If this is true, we know we're at the last cards, so we shouldn't update anything when we move the offset
+        if(parser.AllCards.Length == offset)
+        {
+            length = 0;
+        }
+        // If the offset would be larger than the index of the cards, make it go to the max index
+        else if(parser.AllCards.Length < offset + 8)
+        {
+            length = ((8 + offset) - parser.AllCards.Length);
+        }
+        // Otherwise it's just equal to the lenght of card images (8)
+        else
+        {
+            length = CardImages.Length;
+        }
+        for (int i = 0; i < length; i++)
+        {
+            // Show the cards with their offset, incase we've pressed the arrows
+            CardImages[i].transform.GetChild(0).GetComponent<Text>().text = parser.AllCards[i + offset].CardName;
+        }
+    }
+
+    public void CardsForwards()
+    {
+        // Only add to the offset if it wont make it bigger than our card list
+        if(offset + 8 < parser.AllCards.Length)
+        {
+            offset += 8;
+        }
+            
+        UpdateCards();
+    }
+
+    public void CardsBackwards()
+    {
+        // Make sure we don't make the offset less than zero
+        if(offset - 8 < 0)
+        {
+            offset = 0;
+        }
+        else
+        {
+            offset -= 8;
+        }
+        UpdateCards();
+
+    }
+
+    public void AddToDeck()
+    {
+        int index = -1;
+        for (int i = 0; i < CardImages.Length; i++)
+        {
+            if (EventSystem.current.currentSelectedGameObject == CardImages[i])
+            {
+                index = i;
+                break;
+            }
+        }
+        if(index != -1)
+        {
+
+            deckDisplay.GetComponent<DeckDisplay>().AddCard(parser.AllCards[offset + index]);
+        }
+    }
+}
---
+++ Assets/Scripts/Card Display/DeckDisplay.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public class DeckDisplay : MonoBehaviour {
+
+    // Make a list that stores the deck
+    List<Card> deckList = new List<Card>();
+    [SerializeField] GameObject cardDeckImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateDeckVisuals()
+    {
+        // Sort by mana cost, then by name
+        deckList = deckList.OrderBy(c => deckManaCostToInt(c)).ThenBy(c => c.CardName).ToList();
+
+        List<Tuple<Card, int>> CompactedDeckList = new List<Tuple<Card, int>>();
+
+        for (int i = 0; i < deckList.Count; i++)
+        {
+            // If the last item in the compacted list is the same as the current item in the for loop
+            // We want to add one to the integer in the tuple
+            if(i != 0 && CompactedDeckList[CompactedDeckList.Count - 1].Item1.CardName == deckList[i].CardName)
+            {
+                // Store the old value of the int in the tuple list
+                int oldValue = CompactedDeckList[CompactedDeckList.Count - 1].Item2;
+                // Delete the current tuple at the last index in the list
+                CompactedDeckList.RemoveAt(CompactedDeckList.Count - 1);
+                // Add the new tuple with a properly incremented value
+                CompactedDeckList.Add(new Tuple<Card, int>(deckList[i], ++oldValue));
+            }
+            // If the last card in the compacted list is different from the item currently being added, just add
+            // the new item, with an amount equal to one
+            else
+            {
+                CompactedDeckList.Add(new Tuple<Card, int>(deckList[i], 1));
+            }
+        }
+
+        // Create/Destroy the number of cards necessary based on the new cards added or removed
+        if(transform.childCount < CompactedDeckList.Count)
+        {
+            int length = CompactedDeckList.Count - transform.childCount;
+            // Loop creating new deck card prefabs until the number of those we have equals our decklist count
+            for (int i = 0; i < length; i++)
+            {
+                GameObject card = Instantiate(cardDeckImage, transform);
+            }
+        }
+        else if(transform.childCount > CompactedDeckList.Count)
+        {
+            int length = transform.childCount - CompactedDeckList.Count;
+            // Loop creating new deck card prefabs until the number of those we have equals our decklist count
+            for (int i = 0; i < length; i++)
+            {
+                Destroy(transform.GetChild(i));
+            }
+        }
+
+        // Create, and rename prefabs based on the new card sort
+        for (int i = 0; i < CompactedDeckList.Count; i++)
+        {
+            transform.GetChild(i).GetChild(0).GetComponent<Text>().text = CompactedDeckList[i].Item1.CardName;
+            transform.GetChild(i).GetChild(1).GetComponent<Text>().text = CompactedDeckList[i].Item2.ToString();
+        }
+    }
+
+    int deckManaCostToInt(Card card)
+    {
+        int cost = 0;
+        IEnumerable<char> manaSymbols = card.ManaCost.ToCharArray();
+        foreach (char symbol in manaSymbols)
+        {
+            switch (symbol)
+            {
+                case 'P': case 'R': case 'A': case 'G': case 'Y': cost++; break;
+                default:
+                    cost += (int)char.GetNumericValue(symbol);
+                    break;
+            }
+        }
+        return cost;
+    }
+
+    public void AddCard(Card newCard)
+    {
+        // Add a card to the decklist, and update a visual for it, as well as it's name
+        deckList.Add(newCard);
+        UpdateDeckVisuals();
+
+    }
+
+    public void RemoveCard()
+    {
+
+    }
+}
