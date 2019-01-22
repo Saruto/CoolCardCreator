1ee5263c81e03323f0d2a082193c8a9940a6672d
Shafournee <31718336+Shafournee@users.noreply.github.com>
2019-01-22 00:09:49 -0500

    Added card dragging

---
+++ Assets/Scripts/Play/ClickandDrag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickandDrag : MonoBehaviour
+{
+    bool IsDragging;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsDragging)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+
+    // Function that the button calls which allows us to drag the UI object around
+    public void Drag()
+    {
+        IsDragging = !IsDragging;
+    }
+}
